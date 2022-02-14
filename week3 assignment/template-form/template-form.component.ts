2cc9ac19a10f015632ac76206a5db3d227350b71
revanthrick356 <78248668+revanthrick356@users.noreply.github.com>
2022-02-14 14:45:21 +0530

    Add files via upload

---
+++ week3 assignment/template-form/template-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(logIn:any){
+    console.log(logIn.value);
+    console.log(logIn.status);
+  }
+
+}
