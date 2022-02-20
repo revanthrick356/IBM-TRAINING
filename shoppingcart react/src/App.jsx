94d71b2d8b8ba8eec30af749323b77009ad0b008
revanthrick356 <78248668+revanthrick356@users.noreply.github.com>
2022-02-20 23:32:33 +0530

    Add files via upload

---
+++ shoppingcart react/src/App.jsx
@@ -0,0 +1,49 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Cart from './components/Cart';
+import data from './data';
+import { useState } from 'react';
+
+const App = () => {
+  const { items } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (item) => {
+    const exist = cartItems.find((x) => x.id === item.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === item.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...item, qty: 1 }]);
+    }
+  };
+  const onRemove = (item) => {
+    const exist = cartItems.find((x) => x.id === item.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== item.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === item.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+  return (
+    <div className="App">
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row">
+        <Main items={items} onAdd={onAdd}></Main>
+        <Cart
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        ></Cart>
+      </div>
+    </div>
+  );
+}
+
+export default App;
