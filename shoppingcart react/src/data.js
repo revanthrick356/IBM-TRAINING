dbc61aa3ec09b909113a1fca1db97b4b4f003c5a
revanthrick356 <78248668+revanthrick356@users.noreply.github.com>
2022-02-20 23:34:44 +0530

    Add files via upload

---
+++ shoppingcart react/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Iphone 12',
+        price: 60000,
+        image: 'https://images.unsplash.com/photo-1573920011462-eb3003086611?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      },
+      {
+        id: '2',
+        name: 'Iphone 13',
+        price: 90000,
+        image: 'https://images.unsplash.com/photo-1569183091671-696402586b9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+      },
+      {
+        id: '3',
+        name: 'Iphone 12 Silver',
+        price: 65000,
+        image: 'https://images.unsplash.com/photo-1591337676887-a217a6970a8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+      },
+      {
+        id: '4',
+        name: 'Iphone SE',
+        price: 40000,
+        image: 'https://images.unsplash.com/photo-1510878933023-e2e2e3942fb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
+      },
+      {
+        id: '5',
+        name: 'Iphone 11',
+        price: 50000,
+        image: 'https://images.unsplash.com/photo-1605236453806-6ff36851218e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGlwaG9uZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+      },
+      {
+        id: '6',
+        name: 'Iphone 13 mini',
+        price: 80000,
+        image: 'https://images.unsplash.com/photo-1581795669633-91ef7c9699a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fGlwaG9uZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+      },
+    ],
+  };
+  export default data;
